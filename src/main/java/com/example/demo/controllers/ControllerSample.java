abbc510c84887be1afa51d4eaabce27e969ac3a3
CENCOSUD\Juavilla <juan.villanuevahidalgo@cencosud.cl>
2019-06-23 17:32:08 -0400

    refactor RequestBody

--- src/main/java/com/example/demo/controllers/ControllerSample.java
+++ src/main/java/com/example/demo/controllers/ControllerSample.java
@@ -1,20 +1,20 @@
 package com.example.demo.controllers;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.object.RequestBodySample;
 
 @Controller
 public class ControllerSample {
 	
 	@PostMapping("/helloworld")
 	@ResponseBody
-	public String helloworld(@RequestParam Map<String,String> params) {
-		return "hello "+params.get("name");
+	public String helloworld(@RequestBody RequestBodySample body) {
+		return "hello "+body.getName();
 	}
 	
 	@GetMapping("/LaTerribleApi")
