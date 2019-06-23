d6ff9409676b014e6cfe8c021ccf7be115bb9a98
CENCOSUD\Juavilla <juan.villanuevahidalgo@cencosud.cl>
2019-06-23 16:47:51 -0400

    Spring Example Rest Api with html

---
+++ src/main/java/com/example/demo/ControllerSample.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ControllerSample {
+	
+	@PostMapping("/helloworld")
+	@ResponseBody
+	public String helloworld(@RequestParam Map<String,String> params) {
+		return "hello "+params.get("name");
+	}
+}
